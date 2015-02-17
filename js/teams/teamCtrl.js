c796955fb629bf05a1fd8094b6d5b89adcdde815
Rocky <rockygibson@gmail.com>
2015-02-16 20:11:48 -0700

    progress

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,35 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+
+	$scope.toggleNewGameForm = function() {
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
+
+	if($routeParams.team === 'utahjazz') {
+		$scope.homeTeam = 'Utah Jazz';
+		$scope.logopath = 'images/jazz-logo.png';
+	} else if ($routeParams.team === 'losangeleslakers') {
+		$scope.homeTeam = 'Los Angeles Lakers';
+		$scope.logopath = 'images/lakers-logo.png';
+	} else if ($routeParams.team === 'miamiheat') {
+		$scope.homeTeam = 'Miami Heat';
+		$scope.logopath = 'images/heat-logo.png';
+	}
+
+	$scope.submitGame = function(){
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		teamService.addNewGame($scope.newGame).then(function(){
+			teamService.getTeamData($scope.newGame.homeTeam).then(function(results){
+				$scope.teamData = results;
+				$scope.newGame = {};
+				$scope.showNewGameForm = false;
+			})
+		})
+	}
 
 });
